3dc90e3cc777948d9e6fd70897f6f84a24fd2070
kibigo! <kibigo@ucsb.edu>
2022-11-01 17:10:05 -0700

    Support resource_class_resolver in resource_klass
    See <https://github.com/samvera/valkyrie/commit/3cbccd376cb0b9f623cc125b7b6c9c93a9b35cc3>.

--- lib/valkyrie/sequel/resource_factory/orm_converter.rb
+++ lib/valkyrie/sequel/resource_factory/orm_converter.rb
@@ -38,7 +38,7 @@ module Valkyrie::Sequel
     # Retrieve the Class used to construct the Valkyrie Resource
     # @return [Class]
     def resource_klass
-      internal_resource.constantize
+      Valkyrie.config.resource_class_resolver.call(internal_resource)
     end
 
     # Access the String for the Valkyrie Resource type within the attributes
